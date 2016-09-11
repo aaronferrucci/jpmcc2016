82df275aa25cc4cb366a17145a7255ba3058730d
Aaron Ferrucci <aaron.ferrucci@gmail.com>
2016-09-11 12:25:57 -0700

    File caching, debug version.

--- jpmcc2016.R
+++ jpmcc2016.R
@@ -1,25 +1,45 @@
 
 library(httr)
+library(XML)
 getUrl <- function(bib) {
   url <-
     paste0(
       "https://www.jpmorganchasecc.com/results/search.php",
-      "?city_id=14&series_year=2016?sub_event_id=561&year=2016&search=1",
+      "?city_id=14",
+      "&series_year=2016",
+      "?sub_event_id=561",
+      "&year=2016",
+      "&search=1",
       "&bib=", bib,
-      "&lname=&fname=&team_name=&gender="
+      "&lname=",
+      "&fname=",
+      "&team_name=",
+      "&gender="
     )
 
   return(url)
+}
+
+# input: 29:56, 1:07:48, etc
+# output: time in seconds
+timeToSeconds <- function(time) {
+  nums <- sapply(strsplit(time, ":", fixed=T), FUN=as.numeric)
+  exps <- length(nums):1 - 1
+  mults <- 60^exps
+  s_list <- nums * mults
+  seconds <- sum(s_list)
+  return(seconds)
 }
 
 getData <- function() {
   file <- "jpmcc2016.csv"
   if (file.exists(file)) {
     print(paste0("Using cached data from ", file))
-    allData <- read.csv(file)
+    allData <- read.csv(file, stringsAsFactors=FALSE)
   } else {
     allData <- data.frame()
-    for (i in 1:12000) {
+    # for (i in 1:12000) {
+    for (i in 101:105) {
       url <- getUrl(i)
       resp <- POST(url)
       cont <- content(resp, type="application/x-www-form-urlencoded")
@@ -33,7 +53,22 @@ getData <- function() {
         print(paste0(i, ": nope"))
       }
     }  
+    allData$Name <- as.character(allData$Name)
+    allData$Time <- as.character(allData$Time)
+    allData$Gender <- as.character(allData$Gender)
+    allData$Company <- as.character(allData$Company)
+
+    write.csv(allData, file)
   }
+
+  # Cast some fields to numeric
+  allData$Plc <- as.numeric(allData$Plc)
+  allData$GPlc <- as.numeric(allData$GPlc)
+  allData$Bib <- as.numeric(allData$Bib)
+
+  # time in seconds
+  allData$Time.Seconds <- sapply(allData$Time, timeToSeconds)
+
 
   return(allData)
 }
